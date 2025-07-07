1548b00847900580e74133da9b95e6643de0e6fa
hana <hanaahesham890@gmail.com>
2025-07-07 14:51:54 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/*/.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-8px)' },
+        },
+      },
+      animation: {
+        float: 'float 4s ease-in-out infinite',
+      },
+    }
+
+  },
+  plugins: [],
+}
